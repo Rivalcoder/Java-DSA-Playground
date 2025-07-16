af464f69dff7a7fb08562f8d05ec1f2f4f9dd5be
Rivalcoder <divi242006@gmail.com>
2025-07-16 14:46:13 +0530

    Add files

---
+++ Tree/LevelOrderTraversal.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class BinaryTree {
+    Node root;
+
+    void levelOrder(Node root) {
+        if (root == null) return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node temp = q.poll();
+            System.out.print(temp.data + " ");
+
+            if (temp.left != null) q.add(temp.left);
+            if (temp.right != null) q.add(temp.right);
+        }
+    }
+}
+public class LevelOrderTraversal{
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.root = new Node(1);
+        bt.root.left = new Node(2);
+        bt.root.right = new Node(3);
+        bt.root.left.left = new Node(4);
+        bt.root.left.right = new Node(5);
+        bt.root.right.right = new Node(6);
+
+        System.out.println("Level Order Traversal:");
+        bt.levelOrder(bt.root);
+    }
+}
